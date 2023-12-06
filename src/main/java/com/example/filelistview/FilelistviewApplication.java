5747243a9889e9791560f819fe12912b74f7020a
Arpad Szabo <arpad2000@gmail.com>
2023-12-06 19:39:14 +0100

    Initial commit

---
+++ src/main/java/com/example/filelistview/FilelistviewApplication.java
@@ -0,0 +1,13 @@
+package com.example.filelistview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilelistviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilelistviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/filelistview/FilelistviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.filelistview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilelistviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
