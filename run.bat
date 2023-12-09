6987a66ee0625a6af12a2bb878e8601c3a7201a2
Arpad Szabo <arpad2000@gmail.com>
2023-12-09 14:21:13 +0100

    Final touches

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
-podman run -d --name app1 --network=host -h localhost  luckyman824/flatlistview:db
+podman run -d --name db --network=host -h localhost  luckyman824/flatlistview:db
 podman run -d --name app1 --network=host -h localhost  luckyman824/flatlistview:app1
 podman run -d --name app2 --network=host -h localhost  luckyman824/flatlistview:app2
--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-podman run -d --name app1 --network=host -h localhost  luckyman824/flatlistview:db
+podman run -d --name db --network=host -h localhost  luckyman824/flatlistview:db
 podman run -d --name app1 --network=host -h localhost  luckyman824/flatlistview:app1
 podman run -d --name app2 --network=host -h localhost  luckyman824/flatlistview:app2
